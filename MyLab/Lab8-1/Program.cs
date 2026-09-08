2dc0c652d2e6b77b6f6f481c37f17ecbe7fe3da1
Kunagkone <reenfurrymasor@gmail.com>
2026-09-08 09:47:36 +0700

    feat: update welcome message in Kestrel web server application

--- MyLab/Lab8-1/Program.cs
+++ MyLab/Lab8-1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Welcome to IoT Edge Gateway by [คุณากร มะซอ]!");
 
 app.Run();
