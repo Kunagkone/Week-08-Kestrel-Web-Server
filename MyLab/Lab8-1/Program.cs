f93fc8539e8ee1b4e90c908aeb6e84383356a7c7
Kunagkone <reenfurrymasor@gmail.com>
2026-09-08 09:48:45 +0700

    feat: add API endpoint for gateway status in Kestrel web server application

--- MyLab/Lab8-1/Program.cs
+++ MyLab/Lab8-1/Program.cs
@@ -2,5 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Welcome to IoT Edge Gateway by [คุณากร มะซอ]!");
-
+app.MapGet("/api/status", () => new {
+    gateway = "ESP32-EdgeGateway",
+    status = "Online",
+    uptimeSeconds = Environment.TickCount64 / 1000,
+    isHealthy = true
+});
 app.Run();
