7ebcf31f9a67cd980717be09687fe2b49c3dd471
Kunagkone <reenfurrymasor@gmail.com>
2026-09-08 09:49:32 +0700

    feat: remove default welcome message from Kestrel web server application

--- MyLab/Lab8-1/Program.cs
+++ MyLab/Lab8-1/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Welcome to IoT Edge Gateway by [คุณากร มะซอ]!");
 app.MapGet("/api/status", () => new {
     gateway = "ESP32-EdgeGateway",
     status = "Online",
